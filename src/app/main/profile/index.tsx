5967c8491c296666581378f23b91b42d2b415b2a
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2024-02-13 17:12:34 -0300

    cria página de usuário

--- src/app/main/profile/index.tsx
+++ src/app/main/profile/index.tsx
@@ -1,7 +1,29 @@
 import {Text} from "react-native";
+import {LogOutButton, UserBox, UserContainer, UserEmailBox, UserNameBox} from "../../../styles/profile.style";
+import {push} from "expo-router/build/global-state/routing";
+import {router} from "expo-router";
 
 export default function Profile(){
+
+    const logout = () => {
+        router.push("/")
+    }
+
     return(
-        <Text>Perfil</Text>
+        <UserContainer>
+            <UserBox>
+                <UserNameBox>
+                    <Text style={{fontSize: 20}}>Nome do usuário:</Text>
+                    <Text style={{fontSize: 20}}>Teste Testando Testilson</Text>
+                </UserNameBox>
+                <UserEmailBox>
+                    <Text style={{fontSize: 20}}>Email do usuário:</Text>
+                    <Text style={{fontSize: 20}}>teste@testilson.com</Text>
+                </UserEmailBox>
+                <LogOutButton onPress={logout}>
+                    <Text style={{fontSize: 20, textAlign: "center", color: "#fff"}}>Sair</Text>
+                </LogOutButton>
+            </UserBox>
+        </UserContainer>
     )
 }
